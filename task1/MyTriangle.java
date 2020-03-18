06b68fe7e80c8d8b6e9cedfb65881f2c3c7de520
Vova_1 <lpgmy@yandex.ru>
2020-03-18 22:06:47 +0300

    override equals, hashCode and changed the definition of the triangle type

--- task1/MyTriangle.java
+++ task1/MyTriangle.java
@@ -1,14 +1,18 @@
 package com.company;
 
+import java.lang.reflect.Type;
+import java.util.Objects;
+
 public class MyTriangle {
     private MyPoint v1;
     private MyPoint v2;
     private MyPoint v3;
 
+
     public  MyTriangle(int x1, int y1,int x2, int y2,int x3, int y3){
-        v1.setXY(x1,y1);
-        v2.setXY(x2,y2);
-        v3.setXY(x3,y3);
+        v1 = new MyPoint(x1,y1);
+        v2 = new MyPoint(x2,y2);
+        v3 = new MyPoint(x3,y3);
     }
 
     public MyTriangle(MyPoint v1, MyPoint v2, MyPoint v3) {
@@ -26,6 +30,21 @@ public class MyTriangle {
                 ")]";
     }
 
+    public enum TypeOfTriangle{
+        SCALENE("Scalene"),
+        ISOSCELES("Isosceles"),
+        EQUILATERAL("Equilateral");
+
+        private  String type;
+        TypeOfTriangle(String type){
+            this.type = type;
+        }
+
+        public String getType() {
+            return type;
+        }
+    }
+
     public double getPerimeter() {
         return (v1.distance(v2)+v2.distance(v3)+v3.distance(v1));
     }
@@ -33,14 +52,29 @@ public class MyTriangle {
     public String getType() {
         if (v1.distance(v2)!=v2.distance(v3)) {
             if (v2.distance()!=v3.distance(v1)&v1.distance(v2)!=v3.distance(v1)){
-                return "scalene";
+                return TypeOfTriangle.SCALENE.getType();
             }
             else {
-                return "isosceles";
+                return TypeOfTriangle.ISOSCELES.getType();
             }
         }
         else {
-            return "equilateral";
+            return TypeOfTriangle.EQUILATERAL.getType();
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyTriangle that = (MyTriangle) o;
+        return Objects.equals(v1, that.v1) &&
+                Objects.equals(v2, that.v2) &&
+                Objects.equals(v3, that.v3);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(v1, v2, v3);
     }
 }
